b668a6bae49784d0129ba3ba6a341dd2ef1c5903
myname <sheikhjunaid732000@gmail.com>
2024-05-23 15:16:38 +0530

    payment form page code pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_prj/login_page.dart';
+import 'package:test_prj/payment_form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: PaymentForm(),
     );
   }
 }
---
+++ lib/payment_form.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:test_prj/components/my_button.dart';
+import 'package:test_prj/components/my_hinttext_field.dart';
+
+class PaymentForm extends StatelessWidget {
+  const PaymentForm({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  MyHintTextField(
+                      hintText: Text(
+                    "Name",
+                    style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.w600),
+                  )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey.shade200),
+                            borderRadius: BorderRadius.circular(8)),
+                        hintText: "Card Number",
+                        hintStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey.shade500),
+                        prefixIcon: Icon(
+                          Icons.credit_card_rounded,
+                          color: Colors.grey.shade500,
+                        ),
+                        enabled: true),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  MyHintTextField(
+                      hintText: Text(
+                    "Expiry Date",
+                    style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.w600),
+                  )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  MyHintTextField(
+                      hintText: Text(
+                    "CVV",
+                    style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.w600),
+                  )),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: MyButton(
+                text: "Pay Now",
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
