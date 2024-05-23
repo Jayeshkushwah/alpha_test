233120fd9603bebee9b5a9108c07f9841257056a
myname <sheikhjunaid732000@gmail.com>
2024-05-23 18:30:25 +0530

    pay success page code pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_prj/charging_stations.dart';
+import 'package:test_prj/pay_success_page.dart';
 import 'package:test_prj/vehicle_number.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChargingStations(),
+      home: OrderPlaced(),
     );
   }
 }
---
+++ lib/pay_success_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class OrderPlaced extends StatefulWidget {
+  const OrderPlaced({super.key});
+
+  @override
+  State<OrderPlaced> createState() => _OrderPlacedState();
+}
+
+class _OrderPlacedState extends State<OrderPlaced> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.green, Colors.green],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 120),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/checkbox.png'),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: Container(
+                  height: 200,
+                  width: 350,
+                  alignment: Alignment.center,
+                  color: Colors
+                      .transparent, // Optional overlay color with transparency
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Order Placed Successfully",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 5), // Spacing between texts
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50),
+                        child: Row(children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Text(
+                            "300 Points added to your",
+                            style: TextStyle(color: Colors.white, fontSize: 19),
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      ),
+                      SizedBox(height: 5), // Spacing between texts
+                      Text(
+                        "My Fuels Card",
+                        style: TextStyle(color: Colors.white, fontSize: 19),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
