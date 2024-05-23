7a2372f024019ee21ff50946725c23bdd04caa32
myname <sheikhjunaid732000@gmail.com>
2024-05-23 18:23:41 +0530

    shivani chargingstations page code pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_prj/add_fuel_quantity.dart';
-import 'package:test_prj/payment_form.dart';
+import 'package:test_prj/charging_stations.dart';
 import 'package:test_prj/vehicle_number.dart';
 
 void main() {
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: VehicleNumber(),
+      home: ChargingStations(),
     );
   }
 }
