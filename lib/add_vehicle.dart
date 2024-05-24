e98fb90c6bc04fa1a6d426c38d73235f45f5de26
myname <sheikhjunaid732000@gmail.com>
2024-05-24 15:41:31 +0530

    add vehicle page code

---
+++ lib/add_vehicle.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:test_prj/components/my_button.dart';
+
+class AddVehicles extends StatefulWidget {
+  const AddVehicles({super.key});
+
+  @override
+  State<AddVehicles> createState() => _AddVehiclesState();
+}
+
+class _AddVehiclesState extends State<AddVehicles> {
+  String? selectedValue;
+  final List<String> items = [
+    "Two Wheeler",
+    "Hatchback",
+    "Sedan",
+    "MUV",
+    "SUV"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 130,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.center,
+                  colors: [
+                    Color.fromRGBO(211, 83, 7, 1),
+                    Color.fromRGBO(252, 130, 59, 1),
+                    Color.fromRGBO(252, 130, 59, 1),
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(23),
+                  bottomRight: Radius.circular(23),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 45.0, left: 20),
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 45.0, left: 100),
+                    child: Text(
+                      'Add Vehicles',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 12),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: DropdownButton<String>(
+                        value: selectedValue,
+                        hint: Text('Vehicles Type'),
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        items: items.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            selectedValue = newValue;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SizedBox(
+                height: 53,
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    hintText: 'Asset Capacity/Power (eg: 120kva)',
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 340,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: GestureDetector(
+                child: MyButton(text: 'Next'),
+                onTap: () {
+                  // Navigator.push(context,
+                  //     MaterialPageRoute(builder: (context) => EvVehicle()));
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
