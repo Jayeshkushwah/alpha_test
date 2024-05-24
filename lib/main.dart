7feb721d4edb2caf1d02c63460bb851a050f30fb
myname <sheikhjunaid732000@gmail.com>
2024-05-24 15:51:32 +0530

    other changes not compulsory to add

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_prj/select_address.dart';
-import 'payment_type_page.dart';
+import 'package:test_prj/add_vehicle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SelectAddress(),
+      home: AddVehicles(),
     );
   }
 }
